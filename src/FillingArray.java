4a6a23d3ad6f1e3dba3371afc8e79e17571cd3ae
Dm&try <dmitrypanyukov@gmail.com>
2024-06-03 00:25:11 +0300

    Step 2

--- src/FillingArray.java
+++ src/FillingArray.java
@@ -1,7 +1,9 @@
+import static java.lang.Math.abs;
+
 public class FillingArray {
-    public void fillInArray(Employee[] employee) {
+    static void fillingArray(Employee[] employee) {
         for (int i = 0; i < employee.length; i++) {
-            employee[i] = new Employee(new FIO("Z"+i, "A"+i, "F"+i), 1+i, 50000 *(10+i)/10);
+            employee[i] = new Employee(new FIO("Z" + i, "A" + i, "F" + i), abs(1 + i / 2), 50000 * (10 + i) / 5);
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
-        FIO[] fio = new FIO[10];
-        fio[0] = new FIO("ff", "dd", "gff");
-        fio[1] = new FIO("ee", "aa", "rg");
+import static java.lang.Math.abs;
 
+public class Main {
+    public static void printDataOfAllEmployees(Employee[] employee ) {
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i]);
+        }
+    }
+
+    public static int totalSalary(Employee[] employee) {
+        int totalSalary = 0;
+        for (int i = 0; i < employee.length; i++) {
+            totalSalary += employee[i].getSalary();
+        }
+        return totalSalary;
+    }
+
+    public static int idEmployeeWithMinZP(Employee[] employee) {
+        int absoluteMinimum = 1_000_000;
+        int idEmployeeWithMinZP = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i].getSalary() < absoluteMinimum) {
+                absoluteMinimum = employee[i].getSalary();
+                idEmployeeWithMinZP = employee[i].getId();
+            }
+        }
+        return idEmployeeWithMinZP;
+    }
+
+    public static int idEmployeeWithMaxZP(Employee[] employee) {
+        int absoluteMax = 1_000;
+        int idEmployeeWithMaxZP = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i].getSalary() > absoluteMax) {
+                absoluteMax = employee[i].getSalary();
+                idEmployeeWithMaxZP = employee[i].getId();
+            }
+        }
+        return idEmployeeWithMaxZP;
+    }
+
+    public static void main(String[] args) {
         Employee[] employees = new Employee[10];
-        employees[0] = new Employee(fio[0],3, 100000);
-        employees[1] = new Employee(fio[1], 1,120000);
-        employees[2] = new Employee(new FIO("as", "wq", "er"), 4, 140000);
-        System.out.println(employees[0].getId());
-        System.out.println(employees[2].getFullName());
-        System.out.println(employees[1].getId());
-        System.out.println(employees[2].getId());
-        System.out.println(employees[0]);
+        FillingArray.fillingArray(employees);
+        printDataOfAllEmployees(employees);
+        System.out.println("Сумму затрат на ЗП в месяц равна: " + totalSalary(employees));
+        int id = idEmployeeWithMinZP(employees);
+        System.out.println("Сотрудника с минимальной ЗП: " + employees[id]);
+        id = idEmployeeWithMaxZP(employees);
+        System.out.println("Cотрудника с максимальной ЗП: " + employees[id]);
+
 
     }
 }
