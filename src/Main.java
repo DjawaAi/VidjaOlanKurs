c0bb8df469068b8abf582792e8496be75effc9ce
Dm&try <dmitrypanyukov@gmail.com>
2024-06-03 00:46:00 +0300

    Step 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import static java.lang.Math.abs;
 
 public class Main {
+
     public static void printDataOfAllEmployees(Employee[] employee ) {
         for (int i = 0; i < employee.length; i++) {
             System.out.println(employee[i]);
@@ -39,15 +40,23 @@ public class Main {
         return idEmployeeWithMaxZP;
     }
 
+
+
     public static void main(String[] args) {
         Employee[] employees = new Employee[10];
         FillingArray.fillingArray(employees);
         printDataOfAllEmployees(employees);
+        System.out.println();
         System.out.println("Сумму затрат на ЗП в месяц равна: " + totalSalary(employees));
+
         int id = idEmployeeWithMinZP(employees);
         System.out.println("Сотрудника с минимальной ЗП: " + employees[id]);
+
         id = idEmployeeWithMaxZP(employees);
         System.out.println("Cотрудника с максимальной ЗП: " + employees[id]);
+
+        int averageMonthlySalary = totalSalary(employees) / employees.length;
+        System.out.println("среднее значение зарплат: " + averageMonthlySalary);
 
 
     }
